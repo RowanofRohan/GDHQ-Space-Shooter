4fe847e073cc6149eca129369d5b3d46b0d4b42d
RowanofRohan <49227094+RowanofRohan@users.noreply.github.com>
2021-07-30 22:41:21 -0500

    Phase 2: New Enemy Movement
    Implemented 5 new movement capabilities:
    Default movement upgrade; enemies may now be told to move at any angle.
    Halted Movement
    Burst Movement: Enemies will quickly fly forward, slowing down over time, before flying forward again.
    Serpentine Movement: Enemies will fly down at a steady rate, while travelling back and forth across the screen.
    Circular Movement: Enemies will fly in a large circle.
    
    Also implemented functionality to disable the random element as enemies return to the top of the screen.
    
    Currently, these movement features are only accessible through debug menus; they will be used later as wave systems and new enemy types are implemented.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,16 +4,45 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    //Linear Movement
     [SerializeField]
-    private float speed = 4.0f;
+    private int movementID = 0;
+    [SerializeField]
+    private float verticalSpeed = 4.0f;
+    [SerializeField]
+    private float horizontalSpeed = 0.0f;
+    [SerializeField]
+    private float accelerationFactor = 0.5f;
+    [SerializeField]
+    private bool randomRespawn = true;
+
+    //Specialized Movement
+    [SerializeField]
+    private float initialAngle = 0.0f;
+    [SerializeField]
+    private float currentDirection = 0.0f;
+    [SerializeField]
+    private float movementDelay = 0.0f;
+    [SerializeField]
+    private bool timerActive = false;
+
+    //Internal Variables
+    [SerializeField]
+    private Vector3 destination;
+    [SerializeField]
+    Quaternion rotation;
+
+
+    //Health
     [SerializeField]
     private float maxHealth = 5.0f;
-    [SerializeField]
     private float currentHealth;
 
+    //Item Drops
     [SerializeField]
     private float dropRatePercentage = 20.0f;
 
+    //Screen Bounds
     [SerializeField]
     private float upperScreenBound = 8.0f;
     [SerializeField]
@@ -23,6 +52,7 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private float rightScreenBound = 9.2f;
 
+    //Ojbject Handles
     private Animator enemyAnimator;
     private Collider2D enemyCollider;
     private SpawnManager spawnManager;
@@ -31,6 +61,7 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private int killScore = 100;
 
+    //Audio handles
     private AudioSource audioSource;
     [SerializeField]
     private AudioClip explosionSound;
@@ -41,6 +72,7 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private float laserCooldown = 3.0f;
 
+    //Fire Controller
     private float canFire = -1.0f;
     [SerializeField]
     private float laserMinCD = 3.0f;
@@ -78,6 +110,7 @@ public class Enemy : MonoBehaviour
             Debug.LogError("Player audio source is NULL");
         }
         canFire = Time.time + laserCooldown;
+        destination = transform.position;
     }
 
     void Update()
@@ -94,14 +127,116 @@ public class Enemy : MonoBehaviour
 
     private void MovementController()
     {
-        Vector3 enemyMovement = new Vector3(0,speed*-1,0);
-        transform.Translate(enemyMovement*Time.deltaTime);
+        switch(movementID)
+        {
+            case 0:
+                //Halts
+                break;
+            case 1:
+                //Default Movement
+                DefaultMovement();
+                break;
+            case 2:
+                //Burst Movement
+                BurstMovement();
+                break;
+            case 3:
+                //Serpentine
+                SerpentineMovement();
+                break;
+            case 4:
+                //Circular
+                CircularMovement();
+                break;
+            default:
+                Debug.Log("Bad Movement ID!");
+                break;
+        }
+    }
 
+    private void ScreenBoundCheck()
+    {
         if (transform.position.y <= lowerScreenBound)
         {
-            float randomX = Random.Range(leftScreenBound,rightScreenBound);
-            transform.position = new Vector3(randomX,upperScreenBound - 0.0001f,0);
+            Vector3 distancetoGo = destination - gameObject.transform.position;
+            if (randomRespawn == true)
+            {
+                float randomX = Random.Range(leftScreenBound,rightScreenBound);
+                transform.position = new Vector3(randomX,upperScreenBound - 0.0001f,0);
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x,upperScreenBound - 0.0001f,0);
+            }
+            destination = gameObject.transform.position + distancetoGo;
         }
+    }
+
+    private void DefaultMovement()
+    {
+        //ID: 1
+        Vector3 enemyMovement = new Vector3(0,verticalSpeed*-1,0);
+        rotation = Quaternion.Euler(0,0,initialAngle);
+        transform.Translate(rotation * enemyMovement * Time.deltaTime);
+        destination = gameObject.transform.position;
+
+        ScreenBoundCheck();
+    }
+
+    private void BurstMovement()
+    {
+        //ID: 2
+        if(timerActive != true)
+        {
+            StartCoroutine(BurstMoveTimer());
+        }
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position,destination,accelerationFactor);
+        ScreenBoundCheck();
+    }
+
+    private IEnumerator BurstMoveTimer()
+    {
+        timerActive = true;
+        while(movementID == 2)
+        {
+            Vector3 enemyMovement = new Vector3(0,verticalSpeed*-1,0);
+            rotation = Quaternion.Euler(0,0,initialAngle);
+            destination = gameObject.transform.position + enemyMovement;
+            yield return new WaitForSeconds(movementDelay);
+        }
+        timerActive = false;
+    }
+
+    private void SerpentineMovement()
+    {
+        //ID: 3
+        currentDirection = Mathf.Lerp(horizontalSpeed,horizontalSpeed*-1,Mathf.PingPong(Time.time*accelerationFactor, 1));
+        Vector3 enemyMovement = new Vector3(currentDirection,verticalSpeed*-1,0);
+        transform.Translate(enemyMovement*Time.deltaTime);
+        ScreenBoundCheck();
+
+    }
+
+    private void CircularMovement()
+    {
+        //ID: 4
+        var currentLook = gameObject.transform.rotation;
+        currentDirection = Mathf.Lerp(0.0f,360.0f,Mathf.Repeat(Time.time*accelerationFactor, 1)) - initialAngle;
+        if(currentDirection < 0.0f)
+        {
+            currentDirection += 360.0f;
+        }
+        else if (currentDirection > 360.0f)
+        {
+            currentDirection -= 360.0f;
+        }
+        rotation = Quaternion.Euler(0,0,currentDirection);
+        Vector3 enemyMovement = new Vector3(0,verticalSpeed*-1,0);
+        transform.Translate(rotation * enemyMovement * Time.deltaTime);
+        destination = gameObject.transform.position;
+        gameObject.transform.rotation = currentLook;
+
+        ScreenBoundCheck();
     }
 
     private void FireController()
